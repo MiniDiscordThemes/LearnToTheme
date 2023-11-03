e1fa34a5c2d8d1ead380bedbf2b2011953a2035f
Saltssaumure <themes@saltssaumure.net>
2023-11-03 19:04:48 +0000

    Move headings nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $("main h1, main h2, main h3, main h4, main h5, main h6").each(function () {
+  $("main h3").each(function () {
     $("nav ul").append(
       "<li class='tag-" +
         this.nodeName.toLowerCase() +
